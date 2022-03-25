649a69fe77feaa165b00a213ecc58153008ecd1b
Weinong Wang <weinong@outlook.com>
2022-03-24 20:56:56 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew install jq
+brew install tmux
+brew install neovim
+brew install watch
+brew install ripgrep
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+
+#brew install --casks postman docker
+brew install jesseduffield/lazygit/lazygit
