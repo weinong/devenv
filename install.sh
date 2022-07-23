7399c6c323957257d5211031b18e24304710d048
Weinong Wang <weinong@outlook.com>
2022-07-23 13:50:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ brew install fzf
 $(brew --prefix)/opt/fzf/install
 brew install fd
 brew install go
-brew install zsh-autosuggestions.zsh
+brew install zsh-autosuggestions
 
 #brew install --casks postman docker
 brew install jesseduffield/lazygit/lazygit
